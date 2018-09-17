108bec7687abd547caeefbd8508bf67dadc2e039
Courier <sebastianvaldivita@gmail.com>
2018-09-17 11:22:04 -0300

    Pasando a funciones

--- js/control.js
+++ js/control.js
@@ -1,9 +1,27 @@
 //Programa para controlar el formulario
 
 
+function compruebaDatos(strNombre, strApellido, strMail) {
+	// Checkea el ingreso de datos correctamente
+
+
+	if ((strNombre == "") || (strNombre == "Su nombre")) {
+		// No es el default
+		return false;
+	};
+
+	if ((strApellido == "") || (strApellido == "Su apellido")) {
+		return false;
+	};
+
+	if ((strMail == "") || (strMail == "Su mail")) {
+		return false;
+	};
+};
 
 
 function nombreCompleto(nombre, apellido) {
+	// Forma el nombre y apellido
 	return (nombre + " " + apellido);
 };
 
@@ -13,13 +31,16 @@ function validate() {
 		apellidoInput = document.getElementById("apellido").value,
 		mailInput = document.getElementById("mail").value;
 
-	if ((nombreInput == "") || (apellidoInput == "") || (mailInput == "")) {
-		alert("Ingrese sus datos");
+	//Control
+	var checkInputs = compruebaDatos(nombreInput, apellidoInput, mailInput);
+	if (checkInputs == false) {
+		alert("Ingrese sus datos")
 		return false;
-	};
+	}
+
 
 	var persona = nombreCompleto(nombreInput, apellidoInput);
-
 	alert("Gracias " + persona);
+
 	return true;
 }
