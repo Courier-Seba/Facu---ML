fdf99f39b69d8e5d31f490f1142d233287b042a1
Courier <sebastianvaldivita@gmail.com>
2018-09-17 19:57:03 -0300

    "control nombre andando"

--- js/control.js
+++ js/control.js
@@ -1,22 +1,54 @@
 //Programa para controlar el formulario
 
+//Alertas
+function alertaIncompleto() {
+	return alert("Ingrese sus datos")
+};
+function alertaChistoso() {
+	return alert("Completa bien tus datos")
+};
 
+// Checkea el ingreso de datos correctamente
 function compruebaDatos(strNombre, strApellido, strMail) {
-	// Checkea el ingreso de datos correctamente
 
+	// Chars no aceptados (puden ser más)
+	var charNoAceptado = new Array ("0", "1", "2", "3", "4", "5", "6", "7", "8",
+									"9", "|", "|", "!", "#", "$", "%", "&", "/",
+									"(", ")", "=", "?", "+", "-", "_", ",", ";",
+									":", ".", "*", "@", "{", "}", "[", "]");
+	// Separa los String a una lista de chars
+	var listaLetras = function(someStr) {return someStr.split("")};
 
+	// Control Nombre
 	if ((strNombre == "") || (strNombre == "Su nombre")) {
-		// No es el default
+		// No es el defaul
+		alertaIncompleto();
 		return false;
+	} else {
+		for (i = 0; i < listaLetras(strNombre).length; i++) { 
+			for (j = 0; j < charNoAceptado.length; j++) {
+				if (listaLetras(strNombre)[i] == charNoAceptado[j]) {
+					alertaChistoso();
+					return false;
+				} else { 
+					console.log("Paso");
+				};
+			};
+		};
 	};
 
+	// Control Apellido
 	if ((strApellido == "") || (strApellido == "Su apellido")) {
 		return false;
 	};
 
+	// Control Mail
 	if ((strMail == "") || (strMail == "Su mail")) {
 		return false;
 	};
+
+	//Todo bien
+	return true
 };
 
 
@@ -25,6 +57,8 @@ function nombreCompleto(nombre, apellido) {
 	return (nombre + " " + apellido);
 };
 
+
+// Main 
 function validate() {
 	var 
 		nombreInput = document.getElementById("nombre").value,
@@ -33,8 +67,8 @@ function validate() {
 
 	//Control
 	var checkInputs = compruebaDatos(nombreInput, apellidoInput, mailInput);
+	
 	if (checkInputs == false) {
-		alert("Ingrese sus datos")
 		return false;
 	}
 
